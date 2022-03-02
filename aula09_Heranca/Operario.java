5873685bc941d97e3f022a63b967db43158e6e01
anamenici <anamenici@gmail.com>
2022-03-02 14:23:11 -0300

    Upload aula 09 - Herança

---
+++ aula09_Heranca/Operario.java
@@ -0,0 +1,48 @@
+package aula09_Heranca;
+
+public class Operario extends Pessoa {
+	private double valorProducao;
+	private double comissao;
+	
+	public Operario(String nome, String endereco, String cpf, int telefone, 
+			int idade, double valorProducao, double comissao)
+	{
+		super(nome, endereco, cpf, telefone, idade);
+		this.valorProducao = valorProducao;
+		this.comissao = comissao;
+	}
+
+	public double getValorProducao() {
+		return valorProducao;
+	}
+
+	public void setValorProducao(double valorProducao) {
+		this.valorProducao = valorProducao;
+	}
+
+	public double getComissao() {
+		return comissao;
+	}
+
+	public void setComissao(double comissao) {
+		this.comissao = comissao;
+	}
+	
+	public void imprimirInfo()
+	{
+		System.out.println("\nNome do operrio: " + getNome() + 
+				"\nCPF: " + getCpf() + 
+				"\nIdade: " + getIdade() + 
+				"\nTelefone: " + getTelefone() + 
+				"\nEndereço: " + getEndereco() + 
+				"\nValor monetário dos artigos produzidos: R$" + valorProducao +
+				"\nComissão: R$" + comissao);
+	}
+	
+	public void calcularProducao()
+	{
+		double valorTotal = valorProducao + (valorProducao * (comissao/100));
+		System.out.println("\nO valor total a ser recebido pelo operário:" + getNome() +
+				"é de: R$" + valorTotal);
+	}
+}
---
+++ aula09_Heranca/TesteEmpregado1.java
@@ -0,0 +1,14 @@
+package aula09_Heranca;
+
+public class TesteEmpregado1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Empregado1 emp = new Empregado1("Ana", "Rua da Marola 45", "11122233344", 1187654321, 22, 123, 20000, 15);
+		emp.imprimirInfo();
+		emp.calcularSalario();
+		
+				
+	}
+
+}

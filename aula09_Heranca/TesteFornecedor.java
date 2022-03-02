7aff114a595078a5c3ae6488b7a76ab38f0b8a4b
anamenici <anamenici@gmail.com>
2022-03-02 15:32:52 -0300

    Teste da Classe Fornecedor

---
+++ aula09_Heranca/TesteFornecedor.java
@@ -0,0 +1,11 @@
+package aula09_Heranca;
+
+public class TesteFornecedor {
+
+	public static void main(String[] args) {
+		Fornecedor f1 = new Fornecedor("Álvaro", "Avenida Feliciano Correia, 73", "11122233390", 1156664455, 66, 4, 20);
+		f1.imprimirInfo();
+
+	}
+
+}

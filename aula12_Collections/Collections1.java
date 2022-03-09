75b586a2a74ee80faec32ac2a2003c07d384c997
anamenici <anamenici@gmail.com>
2022-03-09 18:35:34 -0300

    Upload Aula 12 - Collections

---
+++ aula12_Collections/Collections1.java
@@ -0,0 +1,76 @@
+package aula12_Collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+public class Collections1 {
+
+		public static void main(String[] args) {
+			
+			List<Integer> minhaLista = new ArrayList<Integer>();  //criação do List
+			
+			
+			minhaLista.add(2); //inserção dos elementos do meu ArrayList --> minhaLista
+			minhaLista.add(5);
+			minhaLista.add(2);
+			minhaLista.add(3);
+			minhaLista.add(1);
+			minhaLista.add(5);
+			minhaLista.add(8);
+			minhaLista.add(1);
+			//Vou percorrer todos os elementos da minhaLista
+			for(Integer listaElemento:minhaLista)
+			{
+				System.out.println(listaElemento);
+			}
+			
+			System.out.println("\nRemovendo um elemento da lista...");
+			System.out.println();  //limpando o cash de memória
+			
+			minhaLista.remove(0);  
+			
+			for(Integer listaElemento:minhaLista)
+			{
+				System.out.println(listaElemento);
+			}
+			//pegar um elemento da minha lista através do seu índice
+			int primeiroElemento = minhaLista.get(0);
+			System.out.println("\nO primeiro elemento da minha lista é: "+primeiroElemento);
+			
+			System.out.println();//limpando o cash de memória
+			
+			for(int i=0;i<minhaLista.size();i++)
+			{
+				System.out.println("\nElemento: "+minhaLista.get(i));
+			}
+			
+			Collections.sort(minhaLista); //Irá ordenar os elementos da minha lista em ordem crescente
+			System.out.println("\nDepois de ordenada a minha lista, temos: ");
+			System.out.println(minhaLista);
+			
+			System.out.println();//limpando o cash de memória
+			
+			Set<Integer> meuSet = new HashSet<Integer>();
+			
+			meuSet.add(6);
+			meuSet.add(1);
+			meuSet.add(4);
+			meuSet.add(1);
+			meuSet.add(2);
+			meuSet.add(2);
+			
+			Iterator<Integer> iMeuSet = meuSet.iterator();
+			
+			while(iMeuSet.hasNext())
+			{
+				System.out.println(iMeuSet.next());
+			}
+			
+		}
+
+	}
+
